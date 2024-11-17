b43538e8434190c56e4134a6d0baf3f8e841c8ac
Stephen Cheng <zintaen@gmail.com>
2024-11-18 01:07:41 +0700

    fix: remove next/core-web-vitals

--- src/configs/eslint/eslint.nextjs.ts
+++ src/configs/eslint/eslint.nextjs.ts
@@ -3,7 +3,6 @@ import baseConfig from './eslint.base.js';
 export default [
     ...baseConfig,
     {
-        extends: 'next/core-web-vitals',
         ignores: ['.next', 'next.config.js'],
     },
 ];
