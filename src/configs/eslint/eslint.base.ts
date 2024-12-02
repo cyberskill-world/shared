2f35fb45c054fb2949688f576090b90e2af688a8
Stephen Cheng <zintaen@gmail.com>
2024-12-03 01:28:38 +0700

    fix: correct perfectionist/sort-imports rule

--- src/configs/eslint/eslint.base.ts
+++ src/configs/eslint/eslint.base.ts
@@ -10,12 +10,23 @@ export default [
             'no-debugger': 'warn',
             'ts/consistent-type-imports': 'off',
             'ts/consistent-type-exports': 'off',
-            'perfectionist/sort-imports': [
-                'error',
-                {
-                    internalPattern: ['#*', '#*/**'],
-                },
-            ],
+            'perfectionist/sort-imports': ['error', {
+                internalPattern: ['#*', '#*/**'],
+                groups: [
+                    'type',
+                    ['parent-type', 'sibling-type', 'index-type'],
+                    'builtin',
+                    'external',
+                    ['internal', 'internal-type'],
+                    ['parent', 'sibling', 'index'],
+                    'side-effect',
+                    'object',
+                    'unknown',
+                ],
+                newlinesBetween: 'ignore',
+                order: 'asc',
+                type: 'natural',
+            }],
         },
     },
 ];
