dcb429cf85b599acafda71ca8c06af517f22df7f
Stephen Cheng <zintaen@gmail.com>
2025-02-12 01:15:33 +0700

    feat: update libs

--- src/typescript/react.ts
+++ src/typescript/react.ts
@@ -1,9 +1,12 @@
-import type { ApolloClientOptions } from '@apollo/client';
+import type { ApolloCache, ApolloClientOptions } from '@apollo/client';
 import type { JSX, ReactElement, ReactNode } from 'react';
 
 export type T_Children = ReactNode | ReactNode[] | ReactElement | JSX.Element | null;
 
-export type T_ApolloOptions = ApolloClientOptions<unknown> & { url?: string };
+export type T_ApolloOptions = ApolloClientOptions<unknown> & {
+    cache?: ApolloCache<unknown>;
+    url?: string;
+};
 
 export interface I_GraphqlCodegenConfig {
     uri: string;
