13ef5e1ed677ae64eecc027269f997fa5559d0ad
Stephen Cheng <zintaen@gmail.com>
2025-02-25 16:40:44 +0700

    feat: update libs

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => {
     return {
-        entry: ['src/**/*'], // Entry points, specify particular files if needed
-        outDir: 'dist', // Output directory for the compiled files
-        format: ['cjs', 'esm'], // Output formats: CommonJS and ESM
-        target: 'es5', // Target the latest JavaScript (change to 'es2017' or 'es2020' for broader support)
-        splitting: false, // Disable code splitting
-        sourcemap: !!options.watch, // Enable sourcemaps during development (watch mode)
-        minify: !options.watch, // Minify the output in production mode
-        clean: true, // Clean the output directory before bundling
-        dts: true, // Generate TypeScript declaration files (.d.ts)
-        shims: true, // Enable polyfills for `__dirname`, `__filename`, etc.
+        entry: ['src/index.ts', 'src/**/*.ts?(x)'],
+        outDir: 'dist', 
+        format: ['cjs', 'esm'], 
+        target: 'es5', 
+        external: ['react', 'react/jsx-runtime'],
+        sourcemap: !!options.watch,
+        minify: !options.watch,
+        treeshake: true,
+        splitting: true,
+        clean: true,
+        dts: true, 
+        shims: true,
     };
 });
