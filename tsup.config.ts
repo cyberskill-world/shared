ecc70f2b1de8498082d12c00c926cfcba7476216
Stephen Cheng <zintaen@gmail.com>
2025-02-25 16:48:11 +0700

    feat: update libs

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,16 +3,15 @@ import { defineConfig } from 'tsup';
 export default defineConfig((options) => {
     return {
         entry: ['src/index.ts', 'src/**/*.ts?(x)'],
-        outDir: 'dist', 
-        format: ['cjs', 'esm'], 
-        target: 'es5', 
+        outDir: 'dist',
+        format: ['cjs', 'esm'],
+        target: 'es5',
         external: ['react', 'react/jsx-runtime'],
         sourcemap: !!options.watch,
         minify: !options.watch,
-        treeshake: true,
-        splitting: true,
+        splitting: false,
         clean: true,
-        dts: true, 
+        dts: true,
         shims: true,
     };
 });
