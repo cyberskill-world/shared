58a21684ddb02fb68751b3295ed74ab1e951841b
Stephen Cheng <zintaen@gmail.com>
2025-03-12 06:19:37 +0700

    feat: remove commitlint config

--- .commitlintrc.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable antfu/no-import-dist */
-import config from './dist/configs/commitlint/base.js';
-import configs from './dist/configs/index.js';
-
-export default configs.merge('commitlint', config);
--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable antfu/no-import-dist */
-import configs from './dist/configs/index.js';
-import config from './dist/configs/lint-staged/base.js';
-
-export default configs.merge('eslint', config);
