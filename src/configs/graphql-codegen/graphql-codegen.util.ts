28afbaf633fc769ebe1f0b11cfc9b2e7c43af656
Stephen Cheng <zintaen@gmail.com>
2025-04-12 00:39:53 +0700

    feat: remove avoidOptionals

--- src/configs/graphql-codegen/graphql-codegen.util.ts
+++ src/configs/graphql-codegen/graphql-codegen.util.ts
@@ -24,7 +24,6 @@ export function createGraphqlCodegenConfig({
                     preset: 'client',
                     config: {
                         useTypeImports: true,
-                        avoidOptionals: true,
                     },
                 },
             }),
