98e2b7dd058f950afbaa0da6e23e9390a78d194b
Stephen Cheng <zintaen@gmail.com>
2025-04-16 02:40:00 +0700

    feat: remove inlineSources

--- dist/react/apollo-error/apollo-error.provider.d.ts
+++ dist/react/apollo-error/apollo-error.provider.d.ts
@@ -1,1 +1,1 @@
-export { ApolloErrorProvider_alias_3 as ApolloErrorProvider } from '../../_tsup-dts-rollup.js';
+export { ApolloErrorProvider_alias_2 as ApolloErrorProvider } from '../../_tsup-dts-rollup.js';
