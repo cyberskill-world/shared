776d7eb81dfb4350cc00e160c89b10dab83293e3
Stephen Cheng <zintaen@gmail.com>
2025-05-08 03:07:38 +0700

    feat: please work :((

--- src/node/path/path.constant.ts
+++ src/node/path/path.constant.ts
@@ -202,14 +202,14 @@ export const command = {
     }),
     pnpmInstallStandard: buildCommand({
         type: E_CommandType.STRING,
-        command: `${PNPM_CLI} install --ignore-scripts --frozen-lockfile`,
+        command: `${PNPM_CLI} install --ignore-scripts`,
     }),
     pnpmInstallLegacy: buildCommand({
         type: E_CommandType.STRING,
-        command: `${PNPM_CLI} install --ignore-scripts --frozen-lockfile --legacy-peer-deps`,
+        command: `${PNPM_CLI} install --ignore-scripts --legacy-peer-deps`,
     }),
     pnpmInstallForce: buildCommand({
         type: E_CommandType.STRING,
-        command: `${PNPM_CLI} install --ignore-scripts --frozen-lockfile --force`,
+        command: `${PNPM_CLI} install --ignore-scripts --force`,
     }),
 };
