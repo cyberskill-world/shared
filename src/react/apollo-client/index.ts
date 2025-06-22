0ea49a580a773ff7d337cb25338b836ab3c63ce5
Stephen Cheng <zintaen@gmail.com>
2025-06-23 01:57:01 +0700

    feat: export all from @apollo/client/hooks

--- src/react/apollo-client/index.ts
+++ src/react/apollo-client/index.ts
@@ -1,3 +1,5 @@
 export * from './apollo-client.component.js';
 export * from './apollo-client.constant.js';
 export * from './apollo-client.type.js';
+// TODO: change imports to @apollo/client after migration to v4
+export * from '@apollo/client/react/hooks/hooks.cjs';
